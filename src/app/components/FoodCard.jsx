9fd6da6f3d1c1cbc82313cdd9f03afb9a3600fea
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-19 13:42:55 +0600

    Added:FoodCard & FoodDetails

--- src/app/components/FoodCard.jsx
+++ src/app/components/FoodCard.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FoodCard = ({ food }) => {
-  const { dish_name, image_link, alternative_names } = food;
+  const {id, dish_name, image_link, alternative_names } = food;
   return (
     <div className="card bg-base-100 shadow-sm border border-2 border-gray-600">
       <div className="card-body">
@@ -14,6 +15,13 @@ const FoodCard = ({ food }) => {
         width={200} height={200} src={image_link} alt="food"
         ></Image>
       </figure>
+
+      <Link href={`/menu/${id}`}>
+            <div className="bg-gray-700 p-2 border border-gray-600 rounded-md mb-5">
+              Show Details
+            </div>
+      </Link>
+
     </div>
   );
 };
--- src/app/menu/[foodId]/page.jsx
+++ src/app/menu/[foodId]/page.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const FoodDetailPage = async({params}) => {
+const FoodDetailPage = async ({ params }) => {
     const {foodId} = await params
 
-    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods/top-foods')
-    const data = res.json()
-    const {dish_name, image_link, origin_and_popularity} = data.data
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
+    const data = await res.json()
+    const {dish_name, origin_and_popularity} = data.data;
     return (
         <div>
-            <h2>Food Details: {foodId}</h2>
+            <h2>Food Detail: {foodId}</h2>
             <div>
                 <h2>{dish_name}</h2>
             <p>{origin_and_popularity}</p>
