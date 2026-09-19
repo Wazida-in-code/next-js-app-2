248e3a7959dcb56cb0cd709fc26876913a94d9c8
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-19 21:30:00 +0600

    Modified-something

--- src/app/components/SearchFood.jsx
+++ src/app/components/SearchFood.jsx
@@ -2,16 +2,16 @@
 import React, { use, useEffect, useState } from 'react';
 
 const SearchFood = ({foodPromise}) => {
-    const [foods, setFoods] = useState([])
+    // const [foods, setFoods] = useState([])
     // const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods/top-foods')
     // const data = await res.json()
     // const foods = data.data
 
-    // const foods = use(foodPromise).data
+    const foods = use(foodPromise).data;
 
-    useEffect(() => {
-        fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods/top-foods').then(res => res.json()).then(data => setFoods(data.data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods/top-foods').then(res => res.json()).then(data => setFoods(data.data))
+    // }, [])
 
     return (
         <div>
